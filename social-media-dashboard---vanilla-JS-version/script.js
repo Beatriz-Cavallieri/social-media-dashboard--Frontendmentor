ac8cdfe6586695f64547a5e8cc06cf6d4757afb9
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-08-11 23:21:16 -0300

    change file organization

--- social-media-dashboard---vanilla-JS-version/script.js
+++ social-media-dashboard---vanilla-JS-version/script.js
@@ -1,5 +1,5 @@
 // based on solution at https://www.youtube.com/watch?v=ZKXv_ZHQ654
-var checkbox = document.querySelector('input[name=theme');
+var checkbox = document.querySelector('input[name=theme]');
 var container = document.querySelector('.container');
 
 checkbox.addEventListener('change', () => {
@@ -7,11 +7,13 @@ checkbox.addEventListener('change', () => {
         console.log("default");
         document.documentElement.setAttribute('data-theme', 'dark');
         container.classList.add('dark');
+        console.log(container.classList);
         
     }
     else {
         document.documentElement.setAttribute('data-theme', 'default');
         console.log("dark");
         container.classList.remove('dark');
+        
     }
 });
